1a38f2930dcf63bed8db39bb53576503f9a64a3a
Akbar-Nugroho <akbarnugroho12345@gmail.com>
2021-12-19 23:42:46 +0700

    challenge-author

---
+++ app/Models/Book.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Authors;
+
+class Book extends Model
+{
+    use HasFactory;
+    protected $table = "book";
+    protected $fillable = ['title', 'description', 'author', 'publisher', 'date_of_issue'];
+    public function book()
+    {
+        return $this->belongsTo(Authors::class);;
+    }
+}
